6114725437f0972671b20c56ae82ee34728b7d56
Veritas <rose.daemon.belmont@gmail.com>
2021-11-13 14:46:55 -0800

    adds urls and views for splash login signup home

---
+++ capstone/melodic/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #Splashpage path
+    path('', views.Splash.as_view(), name="splash"),
+    #path for login
+    path('login/', views.Login.as_view(), name="login"),
+    #signup
+    path('/signup', views.Signup.as_view(), name="signup"),
+    #home page, shows posts, allows creation of posts
+    path('/home', views.Home.as_view(), name="home"),
+    
+]
--- capstone/melodic/views.py
+++ capstone/melodic/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import UpdateView, CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import DetailView
 
 # Create your views here.
+
+#splashpage 
+class Splash(TemplateView):
+    template_name = 'splash.html'
+
+#login
+class Login(TemplateView):
+    template_name = 'login.html'
+
+#signup
+class Signup(View):
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, "registration/signup.html", context)
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile")
+        else:
+            context = {"form": form}
+            return render(request, "registration/signup.html", context)
+
+#home
+class Home(TemplateView):
+    template_name = 'home.html'
