e3be05f64d8fe753e5dcbce8c1b3ee10285d91f8
Veritas <rose.daemon.belmont@gmail.com>
2021-11-13 15:02:25 -0800

    adds urls and base.html

--- capstone/melodic/urls.py
+++ capstone/melodic/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     #path for login
     path('login/', views.Login.as_view(), name="login"),
     #signup
-    path('/signup', views.Signup.as_view(), name="signup"),
+    path('signup/', views.Signup.as_view(), name="signup"),
     #home page, shows posts, allows creation of posts
-    path('/home', views.Home.as_view(), name="home"),
-    
+    path('home/', views.Home.as_view(), name="home"),
+
 ]
