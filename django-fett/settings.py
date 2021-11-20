627e9950b9440874b7ad02eabc216df2ea572e35
Veritas <rose.daemon.belmont@gmail.com>
2021-11-19 17:59:58 -0800

    renaming

--- django-fett/settings.py
+++ django-fett/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'capstone.urls'
+ROOT_URLCONF = 'django-fett.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'capstone.wsgi.application'
+WSGI_APPLICATION = 'django-fett.wsgi.application'
 
 
 # Database
